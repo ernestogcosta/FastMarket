1163a605b7405736cdb837918c10f74726d3eecf
unknown <andre.henrix17@gmail.com>
2021-05-10 18:54:20 -0300

    Onboarding funcionando, faltam os assets visuais e arrumar os textos.

--- app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
@@ -2,11 +2,26 @@ package br.iesb.mobile.fastmarket.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import br.iesb.mobile.fastmarket.R
+import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var onboard: OnboardingFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    /*
+    private fun setFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_fragments, fragment)
+        fragmentTransaction.commit()
+    }
+    onboard = OnboardingFragment()
+    setFragment(onboard)
+
+    ISSO ACIMA É UMA FUNÇÃO PARA TESTAR ALGUM FRAGMENT ESPECIFICADO NO CÓDIGO.
+    FICA FALTANDO TER O FRAMELAYOUT PARA RECEBER O FRAGMENT NA HORA DE RODAR O APP.
+    */
 }
---
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/fragment/onboarding/OnboardingFragment.kt
@@ -0,0 +1,41 @@
+package br.iesb.mobile.fastmarket.ui.fragment.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.iesb.mobile.fastmarket.databinding.LayoutonboardingBinding
+import br.iesb.mobile.fastmarket.ui.adapter.OnboardingAdapter
+import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingPage1
+import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingPage2
+import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingFragmentPage3
+
+class OnboardingFragment : Fragment() {
+    private lateinit var binding: LayoutonboardingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = LayoutonboardingBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        val pages = arrayListOf(
+            OnboardingPage1(),
+            OnboardingPage2(),
+            OnboardingFragmentPage3()
+        )
+        binding.viewpageOnboarding.adapter = OnboardingAdapter(
+            pages,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        return binding.root
+    }
+    @SuppressWarnings
+    fun start(v: View) {
+
+    }
+}
