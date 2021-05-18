a41ca05a0430cc5993bf4d5628e8d87e7eb143f0
ernestogcosta <ernestogcosta@gmail.com>
2021-05-18 16:50:35 -0300

    .

--- app/src/main/java/br/iesb/mobile/fastmarket/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/fragment/login/LoginFragment.kt
@@ -5,16 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.fastmarket.R
+import br.iesb.mobile.fastmarket.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    val auth = FirebaseAuth.getInstance()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
+
+    fun irSignUp(v: View){
+        findNavController().navigate(R.id.acLoginToSignUp)
+    }
+
+    fun fazerLogin(v: View){
+//        auth.signInWithEmailAndPassword(binding.etEmailLogin.text.toString(), binding.etPasswordLogin.text.toString()).addOnCompleteListener{
+//            if(it.isSuccessful){
+//                Toast.makeText(context, "foi", Toast.LENGTH_LONG).show()
+//                findNavController().navigate(R.id.acLoginToMainActivity)
+//            }else{
+//                Toast.makeText(context, binding.etPasswordLogin.text.toString(), Toast.LENGTH_LONG).show()
+//            }
+//        }
+
+        findNavController().navigate(R.id.acLoginToMainActivity)
+
+    }
+
+    fun irForgot(v: View){
+        findNavController().navigate(R.id.acLoginToForgot)
+    }
+
 
 }
--- app/src/main/java/br/iesb/mobile/fastmarket/ui/fragment/login/SignUpFragment.kt
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/fragment/login/SignUpFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.fastmarket.R
+import br.iesb.mobile.fastmarket.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+
+    fun fazerCadastroELogar(v: View){
+        Toast.makeText(context, getString(R.string.toast_cadastro_realizado), Toast.LENGTH_LONG).show()
+        findNavController().navigate(R.id.acSignUpToMainActivity)
     }
 
 }
