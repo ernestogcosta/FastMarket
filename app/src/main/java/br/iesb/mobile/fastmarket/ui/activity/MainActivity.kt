d2add5f742a778382849ddbbd0661f1e9e1ec5f7
unknown <andre.henrix17@gmail.com>
2021-05-24 02:33:03 -0300

    O bottom navigation menu está ali, mas precisa arrumar o layout. Tambem falta e configurar 100% as funcoes de navegacao.

--- app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
@@ -2,15 +2,25 @@ package br.iesb.mobile.fastmarket.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import br.iesb.mobile.fastmarket.R
 import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 
