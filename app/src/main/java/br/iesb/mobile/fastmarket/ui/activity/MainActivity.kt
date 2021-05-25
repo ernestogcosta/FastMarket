58c7bd4e2e328f7eae6f5b34229deb57684bb0e1
ernestogcosta <ernestogcosta@gmail.com>
2021-05-25 18:34:31 -0300

    Consertando e juntando partes

--- app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
+++ app/src/main/java/br/iesb/mobile/fastmarket/ui/activity/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import br.iesb.mobile.fastmarket.R
 import br.iesb.mobile.fastmarket.ui.fragment.onboarding.OnboardingFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
 
@@ -16,11 +20,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView = findViewById(R.id.bottomNavView)
-        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        var manager = supportFragmentManager
+        var navHost = manager.findFragmentById(R.id.mainNavigationFragment) as NavHostFragment
+        var navController = navHost.navController
+        bottomNavigationView.setupWithNavController(navController)
+//        bottomNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return true
-    }
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        return true
+//    }
 
 }
